6917c8a98cba8fa8516b632831f7ae828e8dd253
declan0873 <declan.c@hotmail.com>
2019-12-28 01:39:28 +0000

    Initial Commit

---
+++ creating-a-notification-centre-widget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  creating-a-notification-centre-widget
+//
+//  Created by Declan on 28/12/2019.
+//  Copyright © 2019 Declan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
